23a398603aa3141c80d98a25b854f537e8f87803
khaledaboshama <khaled.shama-e@maf.ae>
2023-06-05 18:47:02 +0300

    Add client as param in remote data source

--- lib/features/number_trivia/data/datasource/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasource/number_trivia_remote_data_source.dart
@@ -11,18 +11,22 @@ abstract class NumberTriviaRemoteDataSource {
   Future<Either<NumberTriviaModel, Exception>> getConcreteNumberTrivia(
       int number);
   Future<Either<NumberTriviaModel, Exception>> getRandomNumberTrivia();
+  String getNumberConcreteTriviaUrl(int number) => "$baseUrl/$number";
+  String getNumberRandomTriviaUrl() => "$baseUrl/random";
 }
 
 class NumberTriviaRemoteDataSourceImpl extends NumberTriviaRemoteDataSource {
-  String getNumberConcreteTriviaUrl(int number) => "$baseUrl/$number";
-  String getNumberRandomTriviaUrl() => "$baseUrl/random";
+
+  Client client;
+
+  NumberTriviaRemoteDataSourceImpl({required this.client});
 
   @override
   Future<Either<NumberTriviaModel, Exception>> getConcreteNumberTrivia(
       int number) async {
     final url = Uri.parse(getNumberConcreteTriviaUrl(number));
     final response =
-        await get(url, headers: {"Content-Type": "application/json"});
+        await client.get(url, headers: {"Content-Type": "application/json"});
     if (response.statusCode == 200) {
       return Left(NumberTriviaModel.fromJson(jsonDecode(response.body)));
     } else {
@@ -34,7 +38,7 @@ class NumberTriviaRemoteDataSourceImpl extends NumberTriviaRemoteDataSource {
   Future<Either<NumberTriviaModel, Exception>> getRandomNumberTrivia() async {
     final url = Uri.parse(getNumberRandomTriviaUrl());
     final response =
-        await get(url, headers: {"Content-Type": "application/json"});
+        await client.get(url, headers: {"Content-Type": "application/json"});
     if (response.statusCode == 200) {
       return Left(NumberTriviaModel.fromJson(jsonDecode(response.body)));
     } else {
